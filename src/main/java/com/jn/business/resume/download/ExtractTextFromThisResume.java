1bb104570484670d260a6ad74c3b3b691c7b7fc3
onias <onias@noxxonsat.com.br>
2022-06-17 02:33:34 -0300

    .

---
+++ src/main/java/com/jn/business/resume/download/ExtractTextFromThisResume.java
@@ -0,0 +1,30 @@
+package com.jn.business.resume.download;
+
+import com.ccp.decorators.CcpMapDecorator;
+import com.ccp.especifications.text.extractor.CcpTextExtractor;
+import com.ccp.exceptions.process.CcpStepResult;
+import com.ccp.process.CcpNextStepFactory;
+
+class ExtractTextFromThisResume extends CcpNextStepFactory {
+
+	private final CcpTextExtractor textExtractor;
+	
+	
+	
+	public ExtractTextFromThisResume(CcpTextExtractor textExtractor, String businessName) {
+		super(businessName);
+		this.textExtractor = textExtractor;
+	}
+
+
+
+	@Override
+	public CcpStepResult executeDecisionTree(CcpMapDecorator values) {
+		CcpMapDecorator resume = values.getInternalMap("resume");
+		String base64 = resume.getAsString("base64");
+		String resumeText = this.textExtractor.extractText(base64);
+		CcpMapDecorator data = values.put("resumeText", resumeText);
+		return  new CcpStepResult(data, 200, this);
+	}
+
+}
