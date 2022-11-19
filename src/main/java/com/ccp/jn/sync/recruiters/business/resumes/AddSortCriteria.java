fab64131567718298c63080797c306e49ef00af5
onias <onias@noxxonsat.com.br>
2022-11-19 19:58:05 -0300

    GetResumesList, GetResumesStatis

--- src/main/java/com/ccp/jn/sync/recruiters/business/resumes/AddSortCriteria.java
+++ src/main/java/com/ccp/jn/sync/recruiters/business/resumes/AddSortCriteria.java
@@ -1,13 +1,17 @@
 package com.ccp.jn.sync.recruiters.business.resumes;
 
 import com.ccp.decorators.CcpMapDecorator;
+import com.ccp.especifications.db.query.ElasticQuery;
+import com.ccp.especifications.db.query.Must;
 import com.ccp.process.CcpProcess;
 
 public class AddSortCriteria implements CcpProcess{
 
 	@Override
 	public CcpMapDecorator execute(CcpMapDecorator values) {
-		// TODO Auto-generated method stub
+		Must must = values.getAsObject("must");
+		ElasticQuery query = must.endMustAndBackToBool().endBoolAndBackToQuery().endQueryAndBackToRequest();
+		
 		return null;
 	}
 
--- src/main/java/com/ccp/jn/sync/recruiters/controller/ListResumesStats.java
+++ src/main/java/com/ccp/jn/sync/recruiters/controller/ListResumesStats.java
@@ -10,7 +10,6 @@ import com.ccp.jn.sync.recruiters.business.resumes.AddRequiredKeywordsFilter;
 import com.ccp.jn.sync.recruiters.business.resumes.AddSeniorityFilter;
 import com.ccp.jn.sync.recruiters.business.resumes.AddSizeInTheQuery;
 import com.ccp.jn.sync.recruiters.business.resumes.AddSortCriteria;
-import com.ccp.jn.sync.recruiters.business.resumes.AddStatisSpecification;
 import com.ccp.jn.sync.recruiters.business.resumes.AddZeroSizeInTheQuery;
 import com.ccp.jn.sync.recruiters.business.resumes.GetResumesList;
 import com.ccp.jn.sync.recruiters.business.resumes.GetResumesStatis;
@@ -21,24 +20,24 @@ public class ListResumesStats {
 	
 	public Map<String, Object> execute(String recruiter, String json){
 		
-		CcpMapDecorator parameters = new CcpMapDecorator(json);
+		CcpMapDecorator values = new CcpMapDecorator(json);
 		
-		parameters = parameters.whenHasKey(A3D_candidate.ddd.name(), new AddSizeInTheQuery());
-		parameters = parameters.whenHasNotKey(A3D_candidate.ddd.name(), new AddZeroSizeInTheQuery());
-		parameters = parameters.whenHasKey("sort", new AddSortCriteria());
+		values = values.whenHasNotKey(A3D_candidate.ddd.name(), new AddZeroSizeInTheQuery());
+		values = values.whenHasKey(A3D_candidate.ddd.name(), new AddSizeInTheQuery());
+		values = values.whenHasKey("sort", new AddSortCriteria());
 
-		parameters = parameters.whenHasKey(A3D_candidate.seniority.name(), new AddSeniorityFilter());
-		parameters = parameters.whenHasKey("requiredKeywords", new AddRequiredKeywordsFilter());
-		parameters = parameters.whenHasKey("optionalKeywords", new AddOptionalKeywordsFilter());
-		parameters = parameters.whenValueIsTrue(A3D_candidate.pcd.name(), new AddPcdFilter());
-		parameters = parameters.whenHasKey(A3D_candidate.ddd.name(), new AddDddFilter());
+		values = values.whenHasKey(A3D_candidate.seniority.name(), new AddSeniorityFilter());
+		values = values.whenHasKey("requiredKeywords", new AddRequiredKeywordsFilter());
+		values = values.whenHasKey("optionalKeywords", new AddOptionalKeywordsFilter());
+		values = values.whenValueIsTrue(A3D_candidate.pcd.name(), new AddPcdFilter());
+		values = values.whenHasKey(A3D_candidate.ddd.name(), new AddDddFilter());
 
-		parameters = parameters.whenHasNotKey(A3D_candidate.ddd.name(), new AddStatisSpecification());
 
-		parameters = parameters.whenHasNotKey(A3D_candidate.ddd.name(), new GetResumesStatis());
-		parameters = parameters.whenHasKey(A3D_candidate.ddd.name(), new GetResumesList());
+		values = values.whenHasNotKey(A3D_candidate.ddd.name(), new GetResumesStatis());
+		values = values.whenHasKey(A3D_candidate.ddd.name(), new GetResumesList());
 		
+		CcpMapDecorator response = values.getInternalMap("response");
 		//sort(exp, pj,clt, btc, update), from, size, ddd
-		return null;
+		return response.content;
 	}
 }
