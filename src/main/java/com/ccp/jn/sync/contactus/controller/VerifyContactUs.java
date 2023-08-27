a7dfd55fb2014c13005ec3dbc9204ff73552921c
onias <onias@noxxonsat.com.br>
2023-08-27 19:16:29 -0300

    mensagem qualquer

--- src/main/java/com/ccp/jn/sync/contactus/controller/VerifyContactUs.java
+++ src/main/java/com/ccp/jn/sync/contactus/controller/VerifyContactUs.java
@@ -1,10 +1,5 @@
 package com.ccp.jn.sync.contactus.controller;
 
-import com.ccp.decorators.CcpMapDecorator;
-import com.ccp.especifications.db.dao.CalculateId;
-import com.ccp.jn.sync.common.business.JnProcessStatus;
-import com.jn.commons.JnEntity;
-
 public class VerifyContactUs {
 	
 	public void execute(String sender, String subjectType){
