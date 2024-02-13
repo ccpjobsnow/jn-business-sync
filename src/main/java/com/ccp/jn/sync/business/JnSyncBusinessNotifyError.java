23cebdf1f37c7a69191ce7c9d40b3d862645d71d
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-02-12 23:30:46 -0300

    criação do handler genérico

--- src/main/java/com/ccp/jn/sync/business/JnSyncBusinessNotifyError.java
+++ src/main/java/com/ccp/jn/sync/business/JnSyncBusinessNotifyError.java
@@ -1,11 +1,13 @@
 package com.ccp.jn.sync.business;
 
+import java.util.function.Function;
+
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.exceptions.process.CcpAsyncProcess;
 import com.jn.commons.entities.JnEntityAsyncTask;
 import com.jn.commons.utils.JnTopics;
 
-public class JnSyncBusinessNotifyError {
+public class JnSyncBusinessNotifyError implements Function<Throwable, CcpJsonRepresentation> {
 
 	public CcpJsonRepresentation apply(Throwable e) {
 		CcpJsonRepresentation md = new CcpJsonRepresentation(e);
